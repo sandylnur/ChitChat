ec763fb14c4dd6cad58ca87f3695077f3fcc5bc0
Sandyl Nursigadoo <sandylnursigadoo@outlook.com>
2020-05-20 19:42:36 +0400

    Start: Created the Projects for both Server and Client under a ChitChat Solution.

---
+++ ChitChat/ChitChat/ChitChat_Client.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ChitChat
+{
+    partial class ChitChat_Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ChitChat_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 829);
+            this.Name = "ChitChat_Client";
+            this.Text = "ChitChat Client";
+            this.Load += new System.EventHandler(this.ChitChat_Client_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
